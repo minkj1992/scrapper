faf039c51a3cb4971281cb934990093eae83f3c4
minkj1992 <minkj1992@gmail.com>
2021-11-20 15:05:39 +0900

    chore: add debugger utils

---
+++ smoke/smoke/utils.py
@@ -0,0 +1,17 @@
+from rich.console import Console
+from functools import wraps
+
+console = Console()
+
+def print_locals():
+    console.print("print locals()")
+    console.print(locals())
+
+def show_locals(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        result = f(*args, **kw)
+        console.print("show locals()")
+        console.print(locals())
+        return result
+    return wrap
